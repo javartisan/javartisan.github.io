2e6d6a4b6d853a4dcff31acffb297e416e99dfdf
javartisan <javartisan@163.com>
2023-06-26 02:49:02 +0800

    update

--- assets/ScmMainAddSku-4829f63b.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,o as e,c as n}from"./index-c056d14d.js";const o={};function r(t,a){return e(),n("h3",null,"add sku")}const _=c(o,[["render",r]]);export{_ as default};
--- assets/ScmMainManageSku-bc96171f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-c056d14d.js";const a={};function o(r,t){return c(),n("h3",null,"manage sku")}const _=e(a,[["render",o]]);export{_ as default};
