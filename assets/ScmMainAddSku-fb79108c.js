c0c9f5aef665cfa2710409103b352982336d964e
javartisan <javartisan@163.com>
2023-06-25 19:16:36 +0800

    update

--- assets/ScmMainAddSku-fb79108c.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,o as e,c as n}from"./index-52f56b3b.js";const o={};function r(t,a){return e(),n("h3",null,"add sku")}const _=c(o,[["render",r]]);export{_ as default};
--- dist/assets/ScmMainCart-68bbb07b.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,o as e,c as n}from"./index-52f56b3b.js";const r={};function t(o,a){return e(),n("h3",null,"购物车")}const _=c(r,[["render",t]]);export{_ as default};
